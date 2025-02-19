795adbc09f85b7d05f113be695c469c9a6568b43
Luiza Ribeiro <84139409+luizaribeiro06@users.noreply.github.com>
2025-02-19 20:55:51 -0300

    Add files via upload

--- fakers.js
+++ fakers.js
@@ -1,5 +1,6 @@
 import nomes from "./nomes.js"
 import sobrenomes from "./sobrenomes.js"
+import emails from "./email.js"
 
 function rg(){
     return Math.random().toString().slice(3,12)
@@ -11,8 +12,14 @@ function nomeFake(){
 }
 
 function sobrenomeFake(){
-    let index2 = parseInt(Math.random()*sobrenomes.length) //vai percorrer a lista e pegar um index aleatório
+    let index2 = parseInt(Math.random()*sobrenomes.length) 
     return sobrenomes[index2]
 }
+
+function emailFake(nomes){
+    let index3 = parseInt(Math.random()*emails.length) 
+    let dominio = emails[index3]
+    return nomes.toLowerCase()+"@"+dominio
+}
 // console.log(nomes)
-export{rg, nomeFake, sobrenomeFake}
+export{rg, nomeFake, sobrenomeFake, emailFake}
--- user.js
+++ user.js
@@ -1,14 +1,15 @@
 //toda vez que chamar essa função, tem q deolver um objeto com infos do usuario)
 //no JSON, obrigatoriamente os atributos tem que ter aspas duplas
 
-import { rg, nomeFake, sobrenomeFake } from "./fakers.js"
+import { rg, nomeFake, sobrenomeFake, emailFake } from "./fakers.js"
 
 export default function createUser(){
+    let firstName = nomeFake()
     let obj = {
-        firstName: nomeFake(),
+        firstName, //como a variavel esta fora, ele vai puxar automaticamente
         lastName: sobrenomeFake(),
         rg: rg(), //chamando a função rg para gerar aleatório
-        email: "m@m.com"
+        email: emailFake(firstName)
     }
     return obj
 }
